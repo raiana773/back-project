05db880f45598210ba205f9c4aa8cd05db3f2e84
raiana773 <104357057+raiana773@users.noreply.github.com>
2023-07-15 21:43:59 +0545

    project

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -7,104 +7,104 @@ import $axios from "../utils/axios";
 const authContext = createContext();
 
 export function useAuthContext() {
-	return useContext(authContext);
+  return useContext(authContext);
 }
 
 const initialState = {
-	user: null,
+  user: null,
 };
 
 function reducer(state, action) {
-	switch (action.type) {
-		case ACTIONS.user:
-			return { ...state, user: action.payload };
+  switch (action.type) {
+    case ACTIONS.user:
+      return { ...state, user: action.payload };
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 }
 
 function AuthContext({ children }) {
-	const [state, dispatch] = useReducer(reducer, initialState);
-	const navigate = useNavigate();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const navigate = useNavigate();
 
-	async function register(credential) {
-		try {
-			await axios.post(`${BASE_URL}/auth/users/`, credential);
-		} catch (error) {
-			console.log(error);
-		}
-	}
+  async function register(credential) {
+    try {
+      await axios.post(`${BASE_URL}/auth/users/`, credential);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	async function login(credentials) {
-		try {
-			const { data: tokens } = await axios.post(
-				`${BASE_URL}/auth/jwt/create/`,
-				credentials
-			);
-			localStorage.setItem("tokens", JSON.stringify(tokens));
+  async function login(credentials) {
+    try {
+      const { data: tokens } = await axios.post(
+        `${BASE_URL}/auth/jwt/create/`,
+        credentials
+      );
+      localStorage.setItem("tokens", JSON.stringify(tokens));
 
-			const { data } = await $axios.get(`${BASE_URL}/auth/users/me/`);
+      const { data } = await $axios.get(`${BASE_URL}/auth/users/me/`);
 
-			dispatch({
-				type: ACTIONS.user,
-				payload: data,
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	}
+      dispatch({
+        type: ACTIONS.user,
+        payload: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	function logout() {
-		localStorage.removeItem("tokens");
-		dispatch({
-			type: ACTIONS.user,
-			payload: null,
-		});
-	}
+  function logout() {
+    localStorage.removeItem("tokens");
+    dispatch({
+      type: ACTIONS.user,
+      payload: null,
+    });
+  }
 
-	async function checkAuth() {
-		try {
-			const tokens = JSON.parse(localStorage.getItem("tokens"));
-			if (tokens) {
-				const { data } = await $axios.get(`${BASE_URL}/auth/users/me/`);
+  async function checkAuth() {
+    try {
+      const tokens = JSON.parse(localStorage.getItem("tokens"));
+      if (tokens) {
+        const { data } = await $axios.get(`${BASE_URL}/auth/users/me/`);
 
-				dispatch({
-					type: ACTIONS.user,
-					payload: data,
-				});
-			} else {
-				dispatch({
-					type: ACTIONS.user,
-					payload: null,
-				});
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	}
+        dispatch({
+          type: ACTIONS.user,
+          payload: data,
+        });
+      } else {
+        dispatch({
+          type: ACTIONS.user,
+          payload: null,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	async function activateUser(uid, token) {
-		try {
-			await axios.post(`${BASE_URL}/auth/users/activation/`, {
-				uid,
-				token,
-			});
-			navigate("/auth");
-		} catch (error) {
-			console.log(error);
-		}
-	}
+  async function activateUser(uid, token) {
+    try {
+      await axios.post(`${BASE_URL}/auth/users/activation/`, {
+        uid,
+        token,
+      });
+      navigate("/auth");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	const value = {
-		user: state.user,
-		register,
-		login,
-		activateUser,
-		logout,
-		checkAuth,
-	};
-	return <authContext.Provider value={value}>{children}</authContext.Provider>;
+  const value = {
+    user: state.user,
+    register,
+    login,
+    activateUser,
+    logout,
+    checkAuth,
+  };
+  return <authContext.Provider value={value}>{children}</authContext.Provider>;
 }
 
 export default AuthContext;
